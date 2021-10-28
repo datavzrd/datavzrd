37e2b3067af5407d5531262c98cedf7cd3f003a0
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-10-28 15:55:29 +0200

    further work on the skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use structopt::StructOpt;
+
+pub(crate) mod cli;
+pub(crate) mod render;
+
 fn main() {
-    println!("Hello, world!");
+    let opt = cli::Opt::from_args();
+    // call renderer from here.
 }
