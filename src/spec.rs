0f2feaadb4dec4d05b5b233da0cbb81a0f81889c
Felix Wiegand <fxwiegand@gmail.com>
2021-11-04 11:36:35 +0100

    Change config type to yaml

--- src/spec.rs
+++ src/spec.rs
@@ -3,7 +3,7 @@ use derefable::Derefable;
 use serde;
 use serde::de::Deserializer;
 use serde::Deserialize;
-use serde_json;
+use serde_yaml;
 use std::collections::HashMap;
 use std::fs;
 use std::path::{Path, PathBuf};
@@ -14,7 +14,7 @@ pub(crate) struct TablesSpec(#[deref] HashMap<String, TableSpec>);
 impl TablesSpec {
     pub(crate) fn from_file<P: AsRef<Path>>(path: P) -> Result<TablesSpec> {
         let config_file = fs::read_to_string(path)?;
-        Ok(serde_json::from_str(&config_file)?)
+        Ok(serde_yaml::from_str(&config_file)?)
     }
 }
 
@@ -30,7 +30,7 @@ pub(crate) struct TableSpec {
 #[serde(rename_all(deserialize = "kebab-case"))]
 pub(crate) struct RenderColumnSpec {
     #[serde(default)]
-    custom: Option<JSFunction>,
+    custom: Option<String>,
     #[serde(default)]
     link_to_table_row: Option<String>,
     #[serde(default)]
@@ -40,37 +40,11 @@ pub(crate) struct RenderColumnSpec {
     #[serde(default)]
     plot: Option<PlotSpec>,
     #[serde(default)]
-    custom_plot: Option<CustomPlotSpec>,
+    custom_plot: Option<String>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct PlotSpec {
     #[serde(rename = "type")]
     plot_type: String,
-}
-
-/// Placeholder for vega plot specs (we do not need to access their contents in rust)
-#[derive(Default, Debug, Clone)]
-pub(crate) struct CustomPlotSpec;
-
-impl<'de> Deserialize<'de> for CustomPlotSpec {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        Ok(CustomPlotSpec::default())
-    }
-}
-
-/// Placeholder for JS functions (we do not need to access their contents in rust)
-#[derive(Default, Debug, Clone)]
-pub(crate) struct JSFunction;
-
-impl<'de> Deserialize<'de> for JSFunction {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        Ok(JSFunction::default())
-    }
 }
