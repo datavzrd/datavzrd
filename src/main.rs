cb6aa842636f03a054392ec60a294335273e1b67
Felix Wiegand <fxwiegand@gmail.com>
2022-02-15 16:53:08 +0100

    fix: Complete error message for wrong config file path

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crate::render::portable::utils::{render_index_file, render_static_files};
 use crate::render::portable::ItemRenderer;
 use crate::render::Renderer;
 use crate::spec::ItemsSpec;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use structopt::StructOpt;
 
 pub(crate) mod cli;
@@ -12,7 +12,9 @@ pub(crate) mod utils;
 
 fn main() -> Result<()> {
     let opt = cli::Datavzrd::from_args();
-    let config = ItemsSpec::from_file(opt.config).unwrap();
+    let config = ItemsSpec::from_file(&opt.config)
+        .context(format!("Could not find config file under given path {:?}", &opt.config))?;
+
     render_index_file(&opt.output, &config)?;
     render_static_files(&opt.output)?;
 
