4a1bd7e3bad96229aca7208c687d045f624cc468
johanneskoester <johannes.koester@uk-essen.de>
2022-02-17 15:08:41 +0000

    Update example report to commit e813018

--- movies/plots/plot_0.js
+++ movies/plots/plot_0.js
@@ -4,7 +4,7 @@ let plot_0 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"The Silence of the Lambs","value":2},{"key":"Network","value":2},{"key":"Coming Home","value":2},{"key":"On Golden Pond","value":2},{"key":"It Happened One Night","value":2},{"key":"Boys Don't Cry","value":1},{"key":"Sergeant York","value":1},{"key":"Separate Tables","value":1},{"key":"The Favourite","value":1},{"key":"Manchester by the Sea","value":1}]},
+            "data": {"values": [{"key":"The Silence of the Lambs","value":2},{"key":"On Golden Pond","value":2},{"key":"It Happened One Night","value":2},{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"The Lost Weekend","value":1},{"key":"Million Dollar Baby","value":1},{"key":"Mildred Pierce","value":1},{"key":"Min and Bill","value":1},{"key":"Kung Fu Ghandi","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
--- oscars/plots/plot_4.js
+++ oscars/plots/plot_4.js
@@ -4,7 +4,7 @@ let plot_4 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"It Happened One Night","value":2},{"key":"Lincoln","value":1},{"key":"The Queen","value":1},{"key":"Roman Holiday","value":1},{"key":"On the Waterfront","value":1},{"key":"Mildred Pierce","value":1}]},
+            "data": {"values": [{"key":"On Golden Pond","value":2},{"key":"It Happened One Night","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"The Private Life of Henry VIII","value":1},{"key":"Cyrano de Bergerac","value":1},{"key":"Mystic River","value":1},{"key":"The King's Speech","value":1},{"key":"The Champ","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
