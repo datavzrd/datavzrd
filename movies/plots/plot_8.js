40de756cfb92b7a6ac77bdb7e858b8142268804f
johanneskoester <johannes.koester@uk-essen.de>
2022-02-21 09:36:04 +0000

    Update example report to commit b9fca5f

--- movies/plots/plot_8.js
+++ movies/plots/plot_8.js
@@ -4,7 +4,7 @@ let plot_8 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"tt0082846","value":2},{"key":"tt0077362","value":2},{"key":"tt0102926","value":2},{"key":"tt0025316","value":2},{"key":"tt0119822","value":2},{"key":"tt0074958","value":2},{"key":"tt0073486","value":2},{"key":"tt0117631","value":1},{"key":"tt0019788","value":1},{"key":"tt1833116","value":1}]},
+            "data": {"values": [{"key":"tt0082846","value":2},{"key":"tt0102926","value":2},{"key":"tt0077362","value":2},{"key":"tt0119822","value":2},{"key":"tt0025316","value":2},{"key":"tt0074958","value":2},{"key":"tt0073486","value":2},{"key":"tt0109830","value":1},{"key":"tt0081398","value":1},{"key":"tt0022386","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
--- oscars/plots/plot_4.js
+++ oscars/plots/plot_4.js
@@ -4,7 +4,7 @@ let plot_4 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"It Happened One Night","value":2},{"key":"Network","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"On Golden Pond","value":2},{"key":"Coming Home","value":2},{"key":"Jezebel","value":1},{"key":"To Kill a Mockingbird","value":1},{"key":"Room","value":1},{"key":"A Streetcar Named Desire","value":1},{"key":"The Miracle Worker","value":1}]},
+            "data": {"values": [{"key":"Network","value":2},{"key":"It Happened One Night","value":2},{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Coming Home","value":2},{"key":"The Artist","value":1},{"key":"The Song of Bernadette","value":1},{"key":"Darling","value":1},{"key":"The Farmer's Daughter","value":1},{"key":"Moonstruck","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
