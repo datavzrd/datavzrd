ef2cb904b5af0620a16ba3bd7ec326862f5f485b
fxwiegand <fxwiegand@wgdnet.de>
2022-03-07 14:46:08 +0000

    Update example report to commit 16647be

--- oscars/plots/plot_4.js
+++ oscars/plots/plot_4.js
@@ -4,7 +4,7 @@ let plot_4 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"It Happened One Night","value":2},{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"Kung Fu Ghandi","value":1},{"key":"Milk","value":1},{"key":"Funny Girl","value":1},{"key":"A Free Soul","value":1},{"key":"Boys Town","value":1}]},
+            "data": {"values": [{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Coming Home","value":2},{"key":"It Happened One Night","value":2},{"key":"Network","value":2},{"key":"Still Alice","value":1},{"key":"To Kill a Mockingbird","value":1},{"key":"Min and Bill","value":1},{"key":"Dangerous","value":1},{"key":"A Streetcar Named Desire","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
