2de48d0176a8d7e72b7f801dcde948907e1e97b2
johanneskoester <johannes.koester@uk-essen.de>
2022-03-22 12:22:43 +0000

    Update example report to commit 63b527a

--- movies/plots/plot_8.js
+++ movies/plots/plot_8.js
@@ -4,7 +4,7 @@ let plot_8 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"tt0102926","value":2},{"key":"tt0077362","value":2},{"key":"tt0082846","value":2},{"key":"tt0025316","value":2},{"key":"tt0074958","value":2},{"key":"tt0119822","value":2},{"key":"tt0073486","value":2},{"key":"tt1504320","value":1},{"key":"tt0097239","value":1},{"key":"tt0053622","value":1}]},
+            "data": {"values": [{"key":"tt0073486","value":2},{"key":"tt0077362","value":2},{"key":"tt0119822","value":2},{"key":"tt0074958","value":2},{"key":"tt0025316","value":2},{"key":"tt0082846","value":2},{"key":"tt0102926","value":2},{"key":"tt0062994","value":1},{"key":"tt1007029","value":1},{"key":"tt0036855","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
