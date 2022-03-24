c19e5f013c14d784e8e5da569316621fba3c6af1
johanneskoester <johannes.koester@uk-essen.de>
2022-03-24 16:46:43 +0000

    Update example report to commit 542fe4a

--- oscars/plots/plot_3.js
+++ oscars/plots/plot_3.js
@@ -4,7 +4,7 @@ let plot_3 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"Katharine Hepburn","value":4},{"key":"Daniel Day-Lewis","value":3},{"key":"Jack Nicholson","value":2},{"key":"Fredric March","value":2},{"key":"Vivien Leigh","value":2},{"key":"Meryl Streep","value":2},{"key":"Hilary Swank","value":2},{"key":"Glenda Jackson","value":2},{"key":"Jodie Foster","value":2},{"key":"Sean Penn","value":2}]},
+            "data": {"values": [{"key":"Katharine Hepburn","value":4},{"key":"Daniel Day-Lewis","value":3},{"key":"Tom Hanks","value":2},{"key":"Spencer Tracy","value":2},{"key":"Ingrid Bergman","value":2},{"key":"Vivien Leigh","value":2},{"key":"Jodie Foster","value":2},{"key":"Hilary Swank","value":2},{"key":"Luise Rainer","value":2},{"key":"Sally Field","value":2}]},
             "mark": "bar",
             "encoding": {
                 "x": {
