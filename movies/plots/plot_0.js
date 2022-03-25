2606a74040350cb7100819b9c182887d94035e9f
johanneskoester <johannes.koester@uk-essen.de>
2022-03-25 08:01:26 +0000

    Update example report to commit da864de

--- movies/plots/plot_0.js
+++ movies/plots/plot_0.js
@@ -4,7 +4,7 @@ let plot_0 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"It Happened One Night","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"On Golden Pond","value":2},{"key":"Network","value":2},{"key":"Coming Home","value":2},{"key":"Patton","value":1},{"key":"Raging Bull","value":1},{"key":"I Want to Live!","value":1},{"key":"Harry and Tonto","value":1},{"key":"Charly","value":1}]},
+            "data": {"values": [{"key":"It Happened One Night","value":2},{"key":"On Golden Pond","value":2},{"key":"Network","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Coming Home","value":2},{"key":"The Theory of Everything","value":1},{"key":"Anastasia","value":1},{"key":"The Trip to Bountiful","value":1},{"key":"Shakespeare in Love","value":1},{"key":"The Good Earth","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
