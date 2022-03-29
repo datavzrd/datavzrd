34245087e5f5ddf4509347be3d8abbccb2251d94
johanneskoester <johannes.koester@uk-essen.de>
2022-03-29 09:41:52 +0000

    Update example report to commit 224cfd0

--- movies/plots/plot_0.js
+++ movies/plots/plot_0.js
@@ -4,7 +4,7 @@ let plot_0 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"The Silence of the Lambs","value":2},{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"It Happened One Night","value":2},{"key":"On Golden Pond","value":2},{"key":"The Queen","value":1},{"key":"Dr. Jekyll and Mr. Hyde","value":1},{"key":"Disraeli","value":1},{"key":"The Champ","value":1},{"key":"My Fair Lady","value":1}]},
+            "data": {"values": [{"key":"On Golden Pond","value":2},{"key":"Network","value":2},{"key":"It Happened One Night","value":2},{"key":"Coming Home","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Suspicion","value":1},{"key":"The Godfather","value":1},{"key":"Dangerous","value":1},{"key":"The Philadelphia Story","value":1},{"key":"Driving Miss Daisy","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
