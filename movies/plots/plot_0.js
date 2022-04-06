b5d9d0d9cf7db5fe69755a0056484b14f4c3a455
johanneskoester <johannes.koester@uk-essen.de>
2022-04-06 15:10:26 +0000

    Update example report to commit 08388db

--- movies/plots/plot_0.js
+++ movies/plots/plot_0.js
@@ -4,7 +4,7 @@ let plot_0 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"The Silence of the Lambs","value":2},{"key":"On Golden Pond","value":2},{"key":"It Happened One Night","value":2},{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"Hamlet","value":1},{"key":"Driving Miss Daisy","value":1},{"key":"True Grit","value":1},{"key":"Sophie's Choice","value":1},{"key":"Kung Fu Ghandi","value":1}]},
+            "data": {"values": [{"key":"It Happened One Night","value":2},{"key":"Network","value":2},{"key":"Coming Home","value":2},{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Charly","value":1},{"key":"Dead Man Walking","value":1},{"key":"As Good as It Gets","value":1},{"key":"Fargo","value":1},{"key":"Born Yesterday","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
