1ed64a7f461aa41e3b453021c602c3a4ac9070c9
fxwiegand <fxwiegand@wgdnet.de>
2022-05-19 11:40:22 +0000

    Update example report to commit df58cc9

--- movies/plots/plot_6.js
+++ movies/plots/plot_6.js
@@ -4,7 +4,7 @@ let plot_6 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"William Wyler","value":7},{"key":"George Cukor","value":5},{"key":"James L. Brooks","value":3},{"key":"Milos Forman","value":3},{"key":"Daniel Mann","value":3},{"key":"Jonathan Demme","value":3},{"key":"Martin Scorsese","value":3},{"key":"Victor Fleming","value":2},{"key":"Fred Zinnemann","value":2},{"key":"Woody Allen","value":2}]},
+            "data": {"values": [{"key":"William Wyler","value":7},{"key":"George Cukor","value":5},{"key":"Martin Scorsese","value":3},{"key":"James L. Brooks","value":3},{"key":"Daniel Mann","value":3},{"key":"Milos Forman","value":3},{"key":"Jonathan Demme","value":3},{"key":"Robert Benton","value":2},{"key":"Norman Jewison","value":2},{"key":"Hal Ashby","value":2}]},
             "mark": "bar",
             "encoding": {
                 "x": {
--- oscars/plots/plot_4.js
+++ oscars/plots/plot_4.js
@@ -4,7 +4,7 @@ let plot_4 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"Network","value":2},{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"Coming Home","value":2},{"key":"It Happened One Night","value":2},{"key":"The Miracle Worker","value":1},{"key":"Room at the Top","value":1},{"key":"Mary Poppins","value":1},{"key":"Boys Don't Cry","value":1},{"key":"Johnny Belinda","value":1}]},
+            "data": {"values": [{"key":"Network","value":2},{"key":"It Happened One Night","value":2},{"key":"Coming Home","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"On Golden Pond","value":2},{"key":"One Flew Over The Cuckoo's Nest","value":1},{"key":"The Story of Louis Pasteur","value":1},{"key":"A Free Soul","value":1},{"key":"All the King's Men","value":1},{"key":"Driving Miss Daisy","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
