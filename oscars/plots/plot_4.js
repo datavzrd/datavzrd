a26378f3fd821d5d96f9c1ecaf0c797c7b93a03d
johanneskoester <johannes.koester@uni-due.de>
2022-05-23 11:58:35 +0000

    Update example report to commit 3d5990a

--- oscars/plots/plot_4.js
+++ oscars/plots/plot_4.js
@@ -4,7 +4,7 @@ let plot_4 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"Network","value":2},{"key":"It Happened One Night","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"On Golden Pond","value":2},{"key":"Coming Home","value":2},{"key":"Walk the Line","value":1},{"key":"The Story of Louis Pasteur","value":1},{"key":"Sophie's Choice","value":1},{"key":"Coal Miner's Daughter","value":1},{"key":"Suspicion","value":1}]},
+            "data": {"values": [{"key":"Coming Home","value":2},{"key":"Network","value":2},{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"It Happened One Night","value":2},{"key":"7th Heaven","value":1},{"key":"The Great Ziegfeld","value":1},{"key":"Mary Poppins","value":1},{"key":"Patton","value":1},{"key":"The Color of Money","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
