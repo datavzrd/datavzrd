d6cc320856527441c0a71769c523f62383e7717f
fxwiegand <fxwiegand@wgdnet.de>
2022-09-20 10:42:18 +0000

    Update example report to commit d59c6d6

--- movies/plots/plot_8.js
+++ movies/plots/plot_8.js
@@ -4,7 +4,7 @@ let plot_8 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"tt0102926","value":2},{"key":"tt0074958","value":2},{"key":"tt0077362","value":2},{"key":"tt0119822","value":2},{"key":"tt0082846","value":2},{"key":"tt0025316","value":2},{"key":"tt0073486","value":2},{"key":"tt0034248","value":1},{"key":"tt0048563","value":1},{"key":"tt0878804","value":1}]},
+            "data": {"values": [{"key":"tt0074958","value":2},{"key":"tt0077362","value":2},{"key":"tt0082846","value":2},{"key":"tt0025316","value":2},{"key":"tt0102926","value":2},{"key":"tt0119822","value":2},{"key":"tt0073486","value":2},{"key":"tt0035575","value":1},{"key":"tt0081398","value":1},{"key":"tt0405676","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
