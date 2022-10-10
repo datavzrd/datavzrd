4d0a5828c75e129b77df4b2774f84559417a4a05
fxwiegand <fxwiegand@wgdnet.de>
2022-10-10 13:02:21 +0000

    Update example report to commit b076d99

--- movies/plots/plot_0.js
+++ movies/plots/plot_0.js
@@ -4,7 +4,7 @@ let plot_0 = {
     "width": "container",
     "layer": [
         {
-            "data": {"values": [{"key":"The Silence of the Lambs","value":2},{"key":"Network","value":2},{"key":"Coming Home","value":2},{"key":"It Happened One Night","value":2},{"key":"On Golden Pond","value":2},{"key":"All the King's Men","value":1},{"key":"Mystic River","value":1},{"key":"Mary Poppins","value":1},{"key":"Two Women","value":1},{"key":"Monster's Ball","value":1}]},
+            "data": {"values": [{"key":"It Happened One Night","value":2},{"key":"Network","value":2},{"key":"Coming Home","value":2},{"key":"On Golden Pond","value":2},{"key":"The Silence of the Lambs","value":2},{"key":"The Reader","value":1},{"key":"Cat Ballou","value":1},{"key":"Who's Afraid of Virginia Woolf","value":1},{"key":"Rain Man","value":1},{"key":"Suspicion","value":1}]},
             "mark": "bar",
             "encoding": {
                 "x": {
