a77509ac22c9d01ba40d42275013f206ef9b5b3e
Felix Wiegand <fxwiegand@wgdnet.de>
2023-02-08 12:22:21 +0100

    fix: Show ellipsis as tooltip (#350)
    This PR shows an ellipsis as tooltip and therefore also fixes #330.

--- static/datavzrd.js
+++ static/datavzrd.js
@@ -197,7 +197,7 @@ function shortenColumn(ah, columns, title, ellipsis, detail_mode, header_label_l
         function () {
             value = this.innerHTML;
             if (value.length > ellipsis) {
-                this.innerHTML = `${value.substring(0, ellipsis)}<a tabindex="0" role="button" href="#" data-toggle="popover" data-trigger="focus" data-html='true' data-content='<div style="overflow: auto; max-height: 30vh; max-width: 25vw;">${value}</div>'>...</a>`;
+                this.innerHTML = `<span data-toggle="tooltip" data-trigger="hover click focus" title='${value}'>${value.substring(0, ellipsis)}...</span>`;
             }
             row++;
         }
@@ -209,7 +209,7 @@ function shortenHeaderRow(row, ellipsis, skip_label) {
         function() {
             value = this.innerHTML;
             if (value.length > ellipsis && !skip_label) {
-                this.innerHTML = `${value.substring(0, ellipsis)}<a tabindex="0" role="button" href="#" data-toggle="popover" data-trigger="focus" data-html='true' data-content='<div style="overflow: auto; max-height: 30vh; max-width: 25vw;">${value}</div>'>...</a>`;
+                this.innerHTML = `<span data-toggle="tooltip" data-trigger="hover click focus" title='${value}'>${value.substring(0, ellipsis)}...</span>`;
             }
             skip_label = false;
         }
