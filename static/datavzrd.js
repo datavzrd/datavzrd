0f9612396935c6282a8fb727d714046afd392f3e
johanneskoester <johannes.koester@uni-due.de>
2023-04-17 08:20:41 +0000

    Update example report to commit ea53bf2

--- static/datavzrd.js
+++ static/datavzrd.js
@@ -542,15 +542,14 @@ $(document).ready(function() {
                 title += ` <a class="sym" data-toggle="modal" onclick="embedSearch(${config.columns.indexOf(column)})" data-target="#search"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/><path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/></svg></a>`;
             }
 
-            let formatter = function(value, row, index, field) {
-                if (config.format[column] != undefined) {
-                    return config.format[column](value, row, index, field);
+            let formatter = undefined;
+            if (config.format[column] != undefined) {
+                formatter = config.format[column];
+            } else {
+                if (config[column].precision && config[column].is_float) {
+                    formatter = function(value) { return precision_formatter(config[column].precision, value); };
                 } else {
-                    if (config[column].precision && config[column].is_float) {
-                        return precision_formatter(config[column].precision, value);
-                    } else {
-                        return value;
-                    }
+                    formatter = function(value) { return value; };
                 }
             }
 
