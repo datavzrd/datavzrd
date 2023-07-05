8d843b20f1077d51746983e6f7c23a445509cdd6
Felix Wiegand <fxwiegand@wgdnet.de>
2023-07-05 12:29:57 +0200

    fix: Add npm build to build.rs (#466)
    This PR adds the `npm install` command to the build.rs.

--- build.rs
+++ build.rs
@@ -2,9 +2,14 @@ use std::path::PathBuf;
 
 fn main() {
     let work_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("web/");
+    std::process::Command::new("npm")
+        .arg("install")
+        .current_dir(work_dir.to_str().expect("failed to get work dir"))
+        .status()
+        .expect("failed to execute webpack");
     std::process::Command::new("npx")
         .arg("webpack")
         .current_dir(work_dir.to_str().expect("failed to get work dir"))
-        .spawn()
+        .status()
         .expect("failed to execute webpack");
 }
