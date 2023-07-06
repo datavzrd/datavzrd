e0cc12eec7d8f13ca1bcdd4db6ea6757987fc9d2
Felix Wiegand <fxwiegand@wgdnet.de>
2023-07-06 09:02:31 +0200

    build: Modify OUT_DIR instead of source directory for webpack build (#464)
    This PR fixes a build problem where the source directory for the webpack
    build was modified.
    
    ---------
    
    Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>

--- build.rs
+++ build.rs
@@ -1,12 +1,27 @@
 use std::path::PathBuf;
 
 fn main() {
-    let work_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("web/");
+    std::process::Command::new("cp")
+        .args(&[
+            "-r",
+            "web/",
+            PathBuf::from(std::env::var("OUT_DIR").unwrap())
+                .join("web/")
+                .to_str()
+                .unwrap(),
+        ])
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .status()
+        .expect("failed to copy web/ to OUT_DIR/web/");
+
+    let work_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("web/");
+
     std::process::Command::new("npm")
         .arg("install")
         .current_dir(work_dir.to_str().expect("failed to get work dir"))
         .status()
         .expect("failed to execute webpack");
+
     std::process::Command::new("npx")
         .arg("webpack")
         .current_dir(work_dir.to_str().expect("failed to get work dir"))
