31acc771dd15358ec244bc6991f2b53effb3efb5
Felix Wiegand <fxwiegand@wgdnet.de>
2023-08-18 10:52:30 +0200

    fix: Fix histogram embedding

--- web/src/datavzrd.js
+++ web/src/datavzrd.js
@@ -396,6 +396,13 @@ export function embedSearch(index) {
     document.getElementById('search-iframe').setAttribute("src",source);
 }
 
+export function embedHistogram(show_plot, index, plot) {
+    if (show_plot) {
+        vegaEmbed('#plot_${index}', plot)
+    } else {
+        document.getElementById('plot_${index}').innerHTML = '<p>No reasonable plot possible.</p>'
+    }
+}
 function addNumClass(dp_num, ah, detail_mode) {
     for (let i in dp_num) {
         if (dp_num[i]) {
@@ -559,7 +566,7 @@ export function load() {
                 }
 
                 // Add histogram button
-                let histogram_icon = ` <a class="sym" data-toggle="modal" data-target="#modal_${config.columns.indexOf(column)}" onclick="if (show_plot_${config.columns.indexOf(column)}) {vegaEmbed('#plot_${config.columns.indexOf(column)}', plot_${config.columns.indexOf(column)})} else {document.getElementById('plot_${config.columns.indexOf(column)}').innerHTML = '<p>No reasonable plot possible.</p>'}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bar-chart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><rect width="4" height="5" x="1" y="10" rx="1"/><rect width="4" height="9" x="6" y="6" rx="1"/><rect width="4" height="14" x="11" y="1" rx="1"/></svg></a>`;
+                let histogram_icon = ` <a class="sym" data-toggle="modal" data-target="#modal_${config.columns.indexOf(column)}" onclick="datavzrd.embedHistogram(show_plot_${config.columns.indexOf(column)}, ${config.columns.indexOf(column)}, plot_${config.columns.indexOf(column)})"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bar-chart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><rect width="4" height="5" x="1" y="10" rx="1"/><rect width="4" height="9" x="6" y="6" rx="1"/><rect width="4" height="14" x="11" y="1" rx="1"/></svg></a>`;
                 title += histogram_icon;
 
                 // Add static search if not single page mode
