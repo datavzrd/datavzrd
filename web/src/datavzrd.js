c8828c2464ef333ec114a050df74a06c43ae4a9b
Felix Wiegand <fxwiegand@wgdnet.de>
2023-10-13 13:04:50 +0200

    feat: Add special handling for ellipsis: 0 (#553)
    This PR adds special handling for ellipsis: 0 therefore closing #551.

--- web/src/datavzrd.js
+++ web/src/datavzrd.js
@@ -197,7 +197,9 @@ function shortenColumn(ah, columns, title, ellipsis, detail_mode, header_label_l
     $(`table > tbody > tr td:nth-child(${index})`).each(
         function () {
             let value = this.innerHTML;
-            if (value.length > ellipsis) {
+            if (ellipsis === 0) {
+                this.innerHTML = `<div style="width: 100%; height: 100%;" data-toggle="tooltip" data-trigger="hover click focus" title='${value}'></div>`;
+            } else if (value.length > ellipsis) {
                 this.innerHTML = `<span data-toggle="tooltip" data-trigger="hover click focus" title='${value}'>${value.substring(0, ellipsis)}...</span>`;
             }
             row++;
@@ -209,7 +211,9 @@ function shortenHeaderRow(row, ellipsis, skip_label) {
     $(`table > thead > tr:nth-child(${row + 1}) > td`).each(
         function() {
             let value = this.innerHTML;
-            if (value.length > ellipsis && !skip_label) {
+            if (ellipsis === 0 && !skip_label) {
+                this.innerHTML = `<div style="width: 100%; height: 100%;" data-toggle="tooltip" data-trigger="hover click focus" title='${value}'></div>`;
+            } else if (value.length > ellipsis && !skip_label) {
                 this.innerHTML = `<span data-toggle="tooltip" data-trigger="hover click focus" title='${value}'>${value.substring(0, ellipsis)}...</span>`;
             }
             skip_label = false;
