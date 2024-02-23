994f8978f4e292aae3b3caabe40799b6b8612639
Felix Wiegand <fxwiegand@wgdnet.de>
2024-02-23 13:15:21 +0100

    feat: Support json input via readervzrd (#651)
    This PR makes datavzrd support json input via the new `readervzrd` crate
    and also does some major refactoring for the dataset reading with
    datavzrd. The code should be shortened by quite a bit and will be much
    more readable.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,4 @@
-use std::io::Read;
-
+use crate::spec::DatasetSpecs;
 use anyhow::Result;
 
 pub(crate) mod column_index;
@@ -7,10 +6,10 @@ pub(crate) mod column_type;
 pub(crate) mod row_address;
 
 /// Returns the index of the given column of a csv header
-pub(crate) fn column_position<R: Read>(column: &str, reader: &mut csv::Reader<R>) -> Result<usize> {
+pub(crate) fn column_position(column: &str, dataset: &DatasetSpecs) -> Result<usize> {
     // TODO implement something like a lookup table for this, and wrap
     // the csv::Reader in a struct that does this lookup.
-    for (index, c) in reader.headers()?.iter().enumerate() {
+    for (index, c) in dataset.reader()?.headers()?.iter().enumerate() {
         if c == column {
             return Ok(index);
         }
