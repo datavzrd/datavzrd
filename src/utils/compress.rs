9ba7ae2eecfb6d006d432683a19587b3b42cc6a2
fxwiegand <fxwiegand@wgdnet.de>
2024-03-08 08:56:13 +0100

    use msgpack

--- src/utils/compress.rs
+++ src/utils/compress.rs
@@ -6,10 +6,11 @@ use serde_json::{json, Value};
 use rmp_serde::to_vec;
 
 pub(crate) fn compress(data: Value) -> Result<String> {
-    // let mut packer = Packer::new();
-    // let options = PackOptions::new();
-    // let jsonm = packer.pack(&data, &options)?;
-    let msgpack_data = String::from_utf16(&to_vec(&data)?.iter().map(|c| *c as u16).collect_vec())?;
+    dbg!(&data);
+    let mut packer = Packer::new();
+    let options = PackOptions::new();
+    let jsonm = packer.pack(&data, &options)?;
+    let msgpack_data = to_vec(&jsonm)?.iter().map(|&byte| format!("{:08b}", byte)).join("");
     Ok(compress_to_utf16(&msgpack_data))
 }
 
