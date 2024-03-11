datavzrd/datavzrd

commit b62347332a1b15c94742c28f096ce044c938ea0b

perf: Use numeric json types (#668)
This PR makes datavzrd actual use numeric json types for floats and
integers. This is much nesscessary for future updates possibly using
msgpack for better data compression.

src/render/portable/mod.rs
@@ -203,6 +203,8 @@ impl Renderer for ItemRenderer {
                         None
                     };
 
+                    let column_types = classify_table(dataset)?;
+
                     for (page, grouped_records) in &dataset
                         .reader()?
                         .records()?
@@ -229,6 +231,7 @@ impl Renderer for ItemRenderer {
                             self.specs.needs_excel_sheet(),
                             webview_host,
                             &view_sizes,
+                            &column_types,
                             debug,
                         )?;
                     }
@@ -298,6 +301,7 @@ fn render_page<P: AsRef<Path>>(
     has_excel_sheet: bool,
     webview_host: &str,
     view_sizes: &HashMap<String, String>,
+    column_types: &HashMap<String, ColumnType>,
     debug: bool,
 ) -> Result<()> {
     let mut templates = Tera::default();
@@ -334,6 +338,22 @@ fn render_page<P: AsRef<Path>>(
     } else {
         None
     };
+
+    let data = data
+        .iter()
+        .map(|s| {
+            s.iter()
+                .enumerate()
+                .map(|(i, s)| match column_types.get(&titles[i]) {
+                    Some(ColumnType::Float) => json!(s.parse::<f64>().unwrap()),
+                    Some(ColumnType::Integer) => json!(s.parse::<i32>().unwrap()),
+                    _ => {
+                        json!(s.to_string())
+                    }
+                })
+                .collect_vec()
+        })
+        .collect_vec();
 
     let compressed_data = compress(json!(data))?;
 

commit fa629b8f187ae01a90388376b027b29d43c3b0e1

fix: Change integer type to fix possible overflow error (#671)
A quick fix for an error introduced with #668.

src/render/portable/mod.rs
@@ -346,7 +346,7 @@ fn render_page<P: AsRef<Path>>(
                 .enumerate()
                 .map(|(i, s)| match column_types.get(&titles[i]) {
                     Some(ColumnType::Float) => json!(s.parse::<f64>().unwrap()),
-                    Some(ColumnType::Integer) => json!(s.parse::<i32>().unwrap()),
+                    Some(ColumnType::Integer) => json!(s.parse::<i64>().unwrap()),
                     _ => {
                         json!(s.to_string())
                     }