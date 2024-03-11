2c3597d56dde2a1f127cbae9ddab6b156b61e766
fxwiegand <fxwiegand@wgdnet.de>
2024-03-11 14:44:56 +0100

    tests

--- src/utils/compress.rs
+++ src/utils/compress.rs
@@ -6,15 +6,39 @@ use serde_json::{json, Value};
 use rmp_serde::to_vec;
 
 pub(crate) fn compress(data: Value) -> Result<String> {
-    dbg!(&data);
     let mut packer = Packer::new();
     let options = PackOptions::new();
     let jsonm = packer.pack(&data, &options)?;
-    let msgpack_data = to_vec(&jsonm)?.iter().map(|&byte| format!("{:08b}", byte)).join("");
+    let msgpack_data = to_vec(&data)?.iter().map(|&byte| format!("{:08b}", byte)).join("");
     Ok(compress_to_utf16(&msgpack_data))
+}
+
+pub(crate) fn compress_no_msgpack(data: Value) -> Result<String> {
+    let mut packer = Packer::new();
+    let options = PackOptions::new();
+    let jsonm = packer.pack(&data, &options)?;
+    Ok(compress_to_utf16(&jsonm.to_string()))
 }
 
 enum Compression {
     JSONM,
     MESSAGEPACK,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_compress() {
+        let data = json!({
+            "float": "13.489732".parse::<f64>().unwrap(),
+            "another_float": "7.87437848".parse::<f64>().unwrap(),
+        });
+        let compressed = compress(data.clone()).unwrap();
+        println!("{} is the result with msgpack", compressed.chars().count());
+        let compressed_no_msgpack = compress_no_msgpack(data.clone()).unwrap();
+        println!("{} is the result without msgpack.", compressed_no_msgpack.chars().count());
+    }
 }
