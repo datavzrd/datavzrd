d42761b878fe6a1afa2902676d9b8599d6377207
fxwiegand <fxwiegand@wgdnet.de>
2024-03-15 11:38:38 +0000

    Update example report to commit 780018a

--- movies/functions.js
+++ movies/functions.js
@@ -1,1 +1,6 @@
-var custom_func_25c0740391f9f68757894e517ef35361=((a,b)=>{try{const b=parseInt(a.split(` `)[0]);const c=Math.floor(b/60);const d=b%60;return `${c}h ${d}m`}catch(a){datavzrd.custom_error(a,`Runtime`)}})
+function custom_func_25c0740391f9f68757894e517ef35361(value, row) { try { 
+    const total_minutes = parseInt(value.split(' ')[0]);
+    const hours = Math.floor(total_minutes / 60);
+    const minutes = total_minutes % 60;
+    return `${hours}h ${minutes}m`;
+ } catch (e) { datavzrd.custom_error(e, 'Runtime') }}
--- oscars/functions.js
+++ oscars/functions.js
@@ -1,1 +1,8 @@
-var custom_func_bdeb9ccd63a6f2944da7a80b24245f51=((a,b)=>{try{return [{"category":`wins`,"amount":a.split(`/`)[0]},{"category":`nominations`,"amount":a.split(`/`)[1]}]}catch(a){datavzrd.custom_error(a,`overall_wins_and_overall_nominations`)}});var custom_func_681872f53fef3af326344cd4b9202655=(a=>{try{var b=a.birth_mo;const c=[`❄️`,`🌸`,`☀️`,`🍁`];return b<1||b>12?`Invalid month`:c[Math.floor((b%12)/3)]}catch(a){datavzrd.custom_error(a,`birth_season`)}})
+function custom_func_bdeb9ccd63a6f2944da7a80b24245f51(value, row) { try { 
+  return [{"category": "wins", "amount": value.split("/")[0]}, {"category": "nominations", "amount": value.split("/")[1]}]
+ } catch (e) { datavzrd.custom_error(e, 'overall_wins_and_overall_nominations') }}
+function custom_func_681872f53fef3af326344cd4b9202655(row) { try { 
+    var month = row.birth_mo;
+    const seasons = ["❄️", "🌸", "☀️", "🍁"];
+    return (month < 1 || month > 12) ? "Invalid month" : seasons[Math.floor((month % 12) / 3)];
+ } catch (e) { datavzrd.custom_error(e, 'birth_season') }}
