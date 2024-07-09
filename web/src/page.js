7b85dc6b5326cf558dac88c041f223381f506c02
b2230356068 <b2230356068@cs.hacettepe.edu.tr>
2024-07-09 11:52:45 +0300

    refactor: Event Listeners and Functions

--- web/src/page.js
+++ web/src/page.js
@@ -65,17 +65,17 @@ export function render_html_contents() {
         </ol>`;
     let sidebar_html = "";
     if (config.has_excel_sheet) {
-        sidebar_html += '<li class="list-group-item sidebar-btn" id="btnExcel" onclick="window.location=\'../data.xlsx\'">Download excel sheet</li>';
+        sidebar_html += '<li class="list-group-item sidebar-btn" id="btnExcel">Download excel sheet</li>';
     }
     if (config.description) {
         sidebar_html += '<li class="list-group-item sidebar-btn" id="markdown-btn" type="button" data-toggle="collapse" data-target="#collapseDescription" aria-expanded="false" aria-controls="collapseDescription">Show description</li>';
     }
     if (config.is_single_page) {
-        sidebar_html += '<li class="list-group-item sidebar-btn" onclick="datavzrd.downloadCSV()">Download CSV</li>';
+        sidebar_html += '<li class="list-group-item sidebar-btn" id="downloadCSV-btn">Download CSV</li>';
         sidebar_html += '<li class="list-group-item sidebar-btn" id="unhide-btn">Unhide columns</li>';
     }
-    sidebar_html += '<li class="list-group-item sidebar-btn" id="toggleLineNumbers" onclick="datavzrd.toggle_line_numbers()">Show/Hide Line Numbers</li>';
-    sidebar_html += '<li class="list-group-item sidebar-btn" id="screenshotTable" onclick="datavzrd.screenshot_table()">Export table page as SVG</li>';
+    sidebar_html += '<li class="list-group-item sidebar-btn" id="toggleLineNumbers">Show/Hide Line Numbers</li>';
+    sidebar_html += '<li class="list-group-item sidebar-btn" id="screenshotTable">Export table page as SVG</li>';
     const content = `
         <div class="collapse" id="sidebar">
             <div class="card" id="sidebar-card">
