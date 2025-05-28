5ce8fd01b8e8de908e4a72a31bc13327dfb68e79
fxwiegand <fxwiegand@wgdnet.de>
2025-05-28 21:39:16 +0200

    fix: Fix hide icon not working

--- web/src/datavzrd.js
+++ web/src/datavzrd.js
@@ -804,6 +804,7 @@ export function load() {
     $(document).ready(function() {
         document.title = "datavzrd report";
         const columnIndexMap = buildColumnIndexMap();
+        window.columnIndexMap = columnIndexMap;
         render_html_contents();
         $('.table-container').show();
         $('.loading').hide();
@@ -893,7 +894,7 @@ export function load() {
                             <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                         </svg>
                     </div>
-                    <div class="sym hide-sym" onclick="datavzrd.hide(${columnIdMap[column]}, false, columnIndexMap)">
+                    <div class="sym hide-sym" onclick="datavzrd.hide(${columnIdMap[column]}, false, window.columnIndexMap)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
                             <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
                             <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
