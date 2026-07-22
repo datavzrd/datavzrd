5c63cc9d332f8c6d974ff6ffe6e391000b725a68
fxwiegand <fxwiegand@wgdnet.de>
2026-07-22 10:44:14 +0200

    fix: Disable memoize_objects in rust-jsonm to prevent data corruption

--- src/utils/compress.rs
+++ src/utils/compress.rs
@@ -8,7 +8,10 @@ pub(crate) fn compress(data: Value, debug: bool) -> Result<String> {
         return Ok(data.to_string());
     }
     let mut packer = Packer::new();
-    let options = PackOptions::new();
+    let options = PackOptions {
+        memoize_objects: false,
+        ..PackOptions::new()
+    };
     let jsonm = packer.pack(&data, &options)?;
     Ok(json!(compress_to_utf16(&json!(jsonm).to_string())).to_string())
 }
