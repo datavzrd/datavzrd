df90c5be529d57517b6bf2e6d292b62de9aff05e
fxwiegand <fxwiegand@wgdnet.de>
2026-08-31 13:26:55 +0200

    perf: Compute column min and max in a single pass

--- src/render/portable/plot.rs
+++ src/render/portable/plot.rs
@@ -163,21 +163,16 @@ pub(crate) fn get_min_max(
     column_index: usize,
     precision: Option<u32>,
 ) -> Result<(f32, f32)> {
-    let mut min_reader = dataset.reader()?;
-    let mut max_reader = dataset.reader()?;
+    let mut reader = dataset.reader()?;
 
-    let min = min_reader
+    let (min, max) = reader
         .records()?
         .skip(dataset.header_rows - 1)
         .map(|r| r.get(column_index).unwrap().to_string())
-        .filter_map(|s| s.parse().ok())
-        .fold(f32::INFINITY, |a, b| a.min(b));
-    let max = max_reader
-        .records()?
-        .skip(dataset.header_rows - 1)
-        .map(|r| r.get(column_index).unwrap().to_string())
-        .filter_map(|s| s.parse().ok())
-        .fold(f32::NEG_INFINITY, |a, b| a.max(b));
+        .filter_map(|s| s.parse::<f32>().ok())
+        .fold((f32::INFINITY, f32::NEG_INFINITY), |(min, max), v| {
+            (min.min(v), max.max(v))
+        });
 
     if let Some(p) = precision {
         Ok((round(min, p), round(max, p)))
